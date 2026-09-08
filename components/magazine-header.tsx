838fd386a2e79ce79101417bd5fdb2e856d7faca
CityAge <miro@cityage.com>
2026-09-07 17:50:15 -0700

    Use approved sentence case for masthead tagline

--- components/magazine-header.tsx
+++ components/magazine-header.tsx
@@ -16,7 +16,7 @@ export function MagazineHeader({ hideRailOnPhone = false }: { hideRailOnPhone?: 
             <div className="flex flex-col items-center">
               <CityAgeMark id="cityage-masthead" tone="cream" size="large" />
               <p className="mt-4 text-center font-serif text-[13px] md:text-[15px] font-normal leading-snug tracking-[0.04em] text-black/65">
-                Intelligence for the Urban Planet
+                Intelligence for the urban planet
               </p>
             </div>
           </div>
